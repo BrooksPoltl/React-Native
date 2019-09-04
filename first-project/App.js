1982ad4ad403a2405c0d2476bda393ee26a0fc34
BrooksPoltl <bpoltl1@gmail.com>
2019-09-04 17:57:21 -0500

    component

--- first-project/App.js
+++ first-project/App.js
@@ -1,6 +1,6 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
-//testing change
+
 const navigator = createStackNavigator(
   {
     Home: HomeScreen
