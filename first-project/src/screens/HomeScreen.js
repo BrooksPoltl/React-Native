2a8dfdd94e33f97a0101ef518fa0355df9a434f0
BrooksPoltl <bpoltl1@gmail.com>
2019-09-05 19:22:46 -0500

    importing component

--- first-project/src/screens/HomeScreen.js
+++ first-project/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import ComponentsScreen from './ComponentsScreen';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>hello</Text>;
+  return <ComponentsScreen/>
 };
 
 const styles = StyleSheet.create({
