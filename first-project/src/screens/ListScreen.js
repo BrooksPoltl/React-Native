3d61b13d0ccb61e04a8fae43aa3a3427fab6b154
BrooksPoltl <bpoltl1@gmail.com>
2019-09-25 22:52:48 -0500

    flat list

--- first-project/src/screens/ListScreen.js
+++ first-project/src/screens/ListScreen.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = ()=>{
-    return <Text>List Screen</Text>
-};
+    const friends = [
+        { name: 'friend #1' },
+        { name: 'friend #2' },
+        { name: 'friend #3' },
+        { name: 'friend #4' }
+    ]
+    return(
+        <FlatList 
+            data = {friends} 
+            renderItem = {( { item } )=>{
+                return <Text>{item.name}</Text>;
+            }}
+        />
+    )
+}
 
 const styles = StyleSheet.create({
 
